30be054eb350aac8e9662ef9debfc3d90171f28d
fakynno <fakynno@gmail.com>
2022-05-03 22:59:32 -0300

    Tela de prompt das operacoes

---
+++ philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
@@ -0,0 +1,8 @@
+// FUNÇÕES DE UMA CALCULADORA
+
+function calculadora() {
+    const operacao = prompt('Escolha uma operação: \n 1 - Soma (+) \n 2 - Subtração (-) \n ' + 
+    '3 - Multiplicação (*) \n 4 - Divisão real (/) \n 5 - Potenciação (**)');
+}
+
+calculadora();
