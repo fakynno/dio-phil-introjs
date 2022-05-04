c9f4e128f5e7df2ac710fad6796040e2a5e25a54
fakynno <fakynno@gmail.com>
2022-05-03 23:37:34 -0300

    Add validador de operacao

--- philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
+++ philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
@@ -4,75 +4,81 @@ function calculadora() {
     const operacao = Number(prompt('Escolha uma operação: \n 1 - Soma (+) \n 2 - Subtração (-) \n ' + 
     '3 - Multiplicação (*) \n 4 - Divisão real (/) \n 5 - Divisão Inteira (%) \n 6 - Potenciação (**)'));
 
+    if (!operacao || operacao >= 7 || operacao < 1) {
+        alert('Erro - Operação Inválida!');
+        calculadora();
+    } else {
 
-    let numero1 = Number(prompt('Insira o 1o valor'));
-    let numero2 = Number(prompt('Insira o 2o valor'));
-    let resultado;
-
-    if (operacao == 1) {
-        soma();
-    } else if (operacao == 2) {
-        subtracao();
-    } else if (operacao == 3) {
-        multiplicacao();
-    } else if (operacao == 4) {
-        divisaoReal();
-    } else if (operacao == 5) {
-        divisaoInteira();
-    } else if (operacao == 6) {
-        potenciacao();
-    }
-
-    function soma() {
-        resultado = numero1 + numero2;
-        alert(`${numero1} + ${numero2} = ${resultado}`);
-        novaOperacao();
-    }
-
-    function subtracao() {
-        resultado = numero1 - numero2;
-        alert(`${numero1} - ${numero2} = ${resultado}`);
-        novaOperacao();
-    }
-
-    function multiplicacao() {
-        resultado = numero1 * numero2;
-        alert(`${numero1} * ${numero2} = ${resultado}`);
-        novaOperacao();
-    }
-
-    function divisaoReal() {
-        resultado = numero1 / numero2;
-        alert(`${numero1} / ${numero2} = ${resultado}`);        
-        novaOperacao();
-    }
-
-    function divisaoInteira() {
-        resultado = numero1 % numero2;
-        alert(`O resto da divisão entre ${numero1} e ${numero2} é igual a ${resultado}`);
-        novaOperacao();
-    }
-
-    function potenciacao() {
-        resultado = numero1 ** numero2;
-        alert(`${numero1} elevado a ${numero2} é igual a ${resultado}`);
-        novaOperacao();
-    }
-
-    function novaOperacao() {
-
-        let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
-
-        if (opcao == 1) {
-            calculadora();
-        } else if (opcao == 2) {
-            alert('Encerrando o programa...');
-        } else {
-            alert('Digite uma opção válida!');
+        let numero1 = Number(prompt('Insira o 1o valor'));
+        let numero2 = Number(prompt('Insira o 2o valor'));
+        let resultado;
+    
+        if (operacao == 1) {
+            soma();
+        } else if (operacao == 2) {
+            subtracao();
+        } else if (operacao == 3) {
+            multiplicacao();
+        } else if (operacao == 4) {
+            divisaoReal();
+        } else if (operacao == 5) {
+            divisaoInteira();
+        } else if (operacao == 6) {
+            potenciacao();
+        }
+    
+        function soma() {
+            resultado = numero1 + numero2;
+            alert(`${numero1} + ${numero2} = ${resultado}`);
             novaOperacao();
         }
-    }
+    
+        function subtracao() {
+            resultado = numero1 - numero2;
+            alert(`${numero1} - ${numero2} = ${resultado}`);
+            novaOperacao();
+        }
+    
+        function multiplicacao() {
+            resultado = numero1 * numero2;
+            alert(`${numero1} * ${numero2} = ${resultado}`);
+            novaOperacao();
+        }
+    
+        function divisaoReal() {
+            resultado = numero1 / numero2;
+            alert(`${numero1} / ${numero2} = ${resultado}`);        
+            novaOperacao();
+        }
+    
+        function divisaoInteira() {
+            resultado = numero1 % numero2;
+            alert(`O resto da divisão entre ${numero1} e ${numero2} é igual a ${resultado}`);
+            novaOperacao();
+        }
+    
+        function potenciacao() {
+            resultado = numero1 ** numero2;
+            alert(`${numero1} elevado a ${numero2} é igual a ${resultado}`);
+            novaOperacao();
+        }
+    
+        function novaOperacao() {
+    
+            let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+    
+            if (opcao == 1) {
+                calculadora();
+            } else if (opcao == 2) {
+                alert('Encerrando o programa...');
+            } else {
+                alert('Digite uma opção válida!');
+                novaOperacao();
+            }
+        }
+    
+     }
 
- }
+}
 
 calculadora();
