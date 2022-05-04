c36522b5b2a3295e34ce07ff7f1bd77a3e19cf1d
fakynno <fakynno@gmail.com>
2022-05-03 23:13:37 -0300

    add funcao soma sem tratamento

--- philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
+++ philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
@@ -3,24 +3,30 @@
 function calculadora() {
     const operacao = prompt('Escolha uma operação: \n 1 - Soma (+) \n 2 - Subtração (-) \n ' + 
     '3 - Multiplicação (*) \n 4 - Divisão real (/) \n 5 - Potenciação (**)');
-}
 
-let numero1 = prompt('Insira o 1o valor');
-let numero2 = prompt('Insira o 2o valor');
-let resultado;
 
-if (operacao == 1) {
-    soma();
-} else if (operacao == 2) {
-    subtracao();
-} else if (operacao == 3) {
-    multiplicacao();
-} else if (operacao == 4) {
-    divisaoReal();
-} else if (operacao == 5) {
-    divisaoInteira();
-} else if (operacao == 6) {
-    potenciacao();
-}
+    let numero1 = prompt('Insira o 1o valor');
+    let numero2 = prompt('Insira o 2o valor');
+    let resultado;
 
+    function soma() {
+        resultado = numero1 + numero2;
+        alert(`${numero1} + ${numero2} = ${resultado}`);
+    }
+
+        if (operacao == 1) {
+            soma();
+        } else if (operacao == 2) {
+            subtracao();
+        } else if (operacao == 3) {
+            multiplicacao();
+        } else if (operacao == 4) {
+            divisaoReal();
+        } else if (operacao == 5) {
+            divisaoInteira();
+        } else if (operacao == 6) {
+            potenciacao();
+        }
+ }
+ 
 calculadora();
