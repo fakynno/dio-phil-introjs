e79c3477bc04a0cfa12a019e85c283e5425df735
fakynno <fakynno@gmail.com>
2022-05-03 21:10:12 -0300

    uso do switch case

--- philips/intro-javascript/aula 04 - estruturas condicionais/script.js
+++ philips/intro-javascript/aula 04 - estruturas condicionais/script.js
@@ -30,3 +30,17 @@ console.log('*******************');
 
 jogador1 != -1 && jogador2 != -1? console.log('Os jogadores são válidos') : console.log('Jogadores inválidos');
 
+
+// USO DO SWITCH CASE
+
+switch (placar) {
+    case placar = jogador1 > jogador2: 
+        console.log('jogador 1 ganhou');
+        break;
+    case placar = jogador2 > jogador1:
+        console.log('jogador 2 ganhou');
+        break;
+    default:
+        console.log('ninguem ganhou');
+        break;
+}
