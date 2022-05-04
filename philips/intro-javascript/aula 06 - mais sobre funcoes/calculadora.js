9cb8c94f26a42064d77e172fefa60631c6a3a262
fakynno <fakynno@gmail.com>
2022-05-03 23:44:36 -0300

    Add validador secundario

--- philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
+++ philips/intro-javascript/aula 06 - mais sobre funcoes/calculadora.js
@@ -12,7 +12,65 @@ function calculadora() {
         let numero1 = Number(prompt('Insira o 1o valor'));
         let numero2 = Number(prompt('Insira o 2o valor'));
         let resultado;
-    
+
+        if(!numero1 || !numero2) {
+            alert('Erro - parâmetros incorretos!')
+            alert('Voltando ao menu principal...')
+            calculadora();
+        } else {
+
+            function soma() {
+                resultado = numero1 + numero2;
+                alert(`${numero1} + ${numero2} = ${resultado}`);
+                novaOperacao();
+            }
+            
+            function subtracao() {
+                resultado = numero1 - numero2;
+                alert(`${numero1} - ${numero2} = ${resultado}`);
+                novaOperacao();
+            }
+            
+            function multiplicacao() {
+                resultado = numero1 * numero2;
+                alert(`${numero1} * ${numero2} = ${resultado}`);
+                novaOperacao();
+            }
+            
+            function divisaoReal() {
+                resultado = numero1 / numero2;
+                alert(`${numero1} / ${numero2} = ${resultado}`);        
+                novaOperacao();
+            }
+            
+            function divisaoInteira() {
+                resultado = numero1 % numero2;
+                alert(`O resto da divisão entre ${numero1} e ${numero2} é igual a ${resultado}`);
+                novaOperacao();
+            }
+            
+            function potenciacao() {
+                resultado = numero1 ** numero2;
+                alert(`${numero1} elevado a ${numero2} é igual a ${resultado}`);
+                novaOperacao();
+            }
+            
+            function novaOperacao() {
+                
+                let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
+                
+                if (opcao == 1) {
+                    calculadora();
+                } else if (opcao == 2) {
+                    alert('Encerrando o programa...');
+                } else {
+                    alert('Digite uma opção válida!');
+                    novaOperacao();
+                }
+            }
+
+        }        
+
         if (operacao == 1) {
             soma();
         } else if (operacao == 2) {
@@ -25,56 +83,6 @@ function calculadora() {
             divisaoInteira();
         } else if (operacao == 6) {
             potenciacao();
-        }
-    
-        function soma() {
-            resultado = numero1 + numero2;
-            alert(`${numero1} + ${numero2} = ${resultado}`);
-            novaOperacao();
-        }
-    
-        function subtracao() {
-            resultado = numero1 - numero2;
-            alert(`${numero1} - ${numero2} = ${resultado}`);
-            novaOperacao();
-        }
-    
-        function multiplicacao() {
-            resultado = numero1 * numero2;
-            alert(`${numero1} * ${numero2} = ${resultado}`);
-            novaOperacao();
-        }
-    
-        function divisaoReal() {
-            resultado = numero1 / numero2;
-            alert(`${numero1} / ${numero2} = ${resultado}`);        
-            novaOperacao();
-        }
-    
-        function divisaoInteira() {
-            resultado = numero1 % numero2;
-            alert(`O resto da divisão entre ${numero1} e ${numero2} é igual a ${resultado}`);
-            novaOperacao();
-        }
-    
-        function potenciacao() {
-            resultado = numero1 ** numero2;
-            alert(`${numero1} elevado a ${numero2} é igual a ${resultado}`);
-            novaOperacao();
-        }
-    
-        function novaOperacao() {
-    
-            let opcao = prompt('Deseja fazer outra operação?\n 1 - Sim\n 2 - Não');
-    
-            if (opcao == 1) {
-                calculadora();
-            } else if (opcao == 2) {
-                alert('Encerrando o programa...');
-            } else {
-                alert('Digite uma opção válida!');
-                novaOperacao();
-            }
         }
     
      }
