f8312c1301274e5026af30506b4320d3b1fcc061
fakynno <fakynno@gmail.com>
2022-05-03 22:38:15 -0300

    adicionando funcoes

--- philips/intro-javascript/aula 05 - funcoes/script.js
+++ philips/intro-javascript/aula 05 - funcoes/script.js
@@ -5,3 +5,15 @@ function novaFuncao() {
 }
 
 novaFuncao();
+
+
+// USO DE FUNÇÕES COM PARÂMETROS
+
+function soma(a, b) {
+    console.log('resultado: ' + (a+b));
+}
+
+valor1 = 12;
+valor2 = 25;
+
+console.log('soma: ' + soma(valor1, valor2));
