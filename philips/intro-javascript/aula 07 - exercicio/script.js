ef74e256236b7a6e32bb637dea8a0d6ff014d076
fakynno <fakynno@gmail.com>
2022-05-04 23:09:49 -0300

    Explicacao da atividade

---
+++ philips/intro-javascript/aula 07 - exercicio/script.js
@@ -0,0 +1,7 @@
+// exercício prático
+
+// crie uma função que recebe 2 números como parâmetros
+// confira se os números são iguais
+// confira se a soma dos número é maior que 10 ou menor que 20
+// retorne uma string dizendo: "os números 'num1' e 'num2' não/são iguais. sua soma é 
+// 'soma', que é 'maior/menor' que 20
