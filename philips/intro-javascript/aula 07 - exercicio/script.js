5a5ce5399b9671580a56e5b3aaa538e550569cea
fakynno <fakynno@gmail.com>
2022-05-05 19:50:16 -0300

    adicao da logica

--- philips/intro-javascript/aula 07 - exercicio/script.js
+++ philips/intro-javascript/aula 07 - exercicio/script.js
@@ -4,7 +4,7 @@
 // confira se os números são iguais
 // confira se a soma dos número é maior que 10 ou menor que 20
 // retorne uma string dizendo: "os números 'num1' e 'num2' não/são iguais. sua soma é 
-// 'soma', que é 'maior/menor' que 20
+// 'soma', que é 'maior/menor' que 10 e 'menor/maior' que 20
 
 function comparaNumeros() {
     
@@ -14,7 +14,8 @@ function comparaNumeros() {
     
     let resultado = numero1 == numero2? alert(`Os números ${numero1} e ${numero2} são iguais`) 
     : alert(`Os números ${numero1} e ${numero2} não são iguais`);
-    alert(`Sua soma é ${numero1 + numero2}, que é ${(numero1 + numero2 > 20? 'maior' : 'menor')} que 20`);
+    alert(`Sua soma é ${numero1 + numero2}, que é ${(numero1 + numero2 > 10? 'maior' : 'menor')} que 10 e 
+    ${(numero1 + numero2 > 20? 'maior' : 'menor')} que 20`);
     
     // const opcao = Number(prompt('Informe um número: '));
 
