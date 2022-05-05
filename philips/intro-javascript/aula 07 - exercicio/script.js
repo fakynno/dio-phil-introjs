892035a33798c3670e0cd681812f88db10ec0046
fakynno <fakynno@gmail.com>
2022-05-04 23:26:33 -0300

    Resolucao do exercicio 07 com Template Strings

--- philips/intro-javascript/aula 07 - exercicio/script.js
+++ philips/intro-javascript/aula 07 - exercicio/script.js
@@ -5,3 +5,19 @@
 // confira se a soma dos número é maior que 10 ou menor que 20
 // retorne uma string dizendo: "os números 'num1' e 'num2' não/são iguais. sua soma é 
 // 'soma', que é 'maior/menor' que 20
+
+function comparaNumeros() {
+    
+    let numero1 = Number(prompt('Informe um número: '));
+    let numero2 = Number(prompt('Informe outro número: '));
+    let soma = numero1 + numero2;
+    
+    let resultado = numero1 == numero2? alert(`Os números ${numero1} e ${numero2} são iguais`) 
+    : alert(`Os números ${numero1} e ${numero2} não são iguais`);
+    alert(`Sua soma é ${numero1 + numero2}, que é ${(numero1 + numero2 > 20? 'maior' : 'menor')} que 20`);
+    
+    // const opcao = Number(prompt('Informe um número: '));
+
+}
+
+comparaNumeros();
