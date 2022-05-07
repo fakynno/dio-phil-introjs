38a6f29e079267a396ef01283a76012f20f29a12
fakynno <fakynno@gmail.com>
2022-05-07 18:50:50 -0300

    Instrucoes da atividade a ser feita

---
+++ philips/intro-javascript/aula 11 - modulos/funcoes.mjs
@@ -0,0 +1,4 @@
+// crie uma função chamada mostraIdade, que recebe 'nome' e 'idade' e retorna a string:
+// `a idade de ${} é ${}`
+// siga o mesmo padrão para outras funções como 'mostraCidade' e 'mostraHobby'
+// exporte essas funções
