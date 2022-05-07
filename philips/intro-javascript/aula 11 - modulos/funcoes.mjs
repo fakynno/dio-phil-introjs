8390df16679c76bb7a0d1804b500d429719d7757
fakynno <fakynno@gmail.com>
2022-05-07 19:13:33 -0300

    Add funcoes e export

--- philips/intro-javascript/aula 11 - modulos/funcoes.mjs
+++ philips/intro-javascript/aula 11 - modulos/funcoes.mjs
@@ -2,3 +2,19 @@
 // `a idade de ${} é ${}`
 // siga o mesmo padrão para outras funções como 'mostraCidade' e 'mostraHobby'
 // exporte essas funções
+
+function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+    "use strict";    
+    return `A cidade de ${nome} se chama ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `O hobby de ${nome} é ${hobby}`;
+}
+
+export { mostraIdade, mostraCidade,mostraHobby };
+
