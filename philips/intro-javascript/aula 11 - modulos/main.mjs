7f094460447db324908c42b4930ae2392de05d0f
fakynno <fakynno@gmail.com>
2022-05-07 19:30:55 -0300

    Chamada das funcoes atraves do import

---
+++ philips/intro-javascript/aula 11 - modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Fabio", 40));
+console.log(mostraCidade("Fabio", "São Paulo"));
+console.log(mostraHobby("Fabio", "Jogar video game"));
