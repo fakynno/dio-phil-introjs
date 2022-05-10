c4ffd4053db094a45f474c8b9619cbb376434086
fakynno <fakynno@gmail.com>
2022-05-09 23:58:55 -0300

    Atividade set

--- philips/intro-javascript/aula 13 - colecoes/atividadesets.js
+++ philips/intro-javascript/aula 13 - colecoes/atividadesets.js
@@ -1,1 +1,30 @@
 // Dado o array [30, 30, 40, 5, 223, 2049, 3034, 5], retorne outro array apenas com valores únicos
+
+const arrayDuplicados = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function removeDuplicados(arrayDuplicados) {
+    let novoArray = new Set();
+    
+    for (let i = 0; i < arrayDuplicados.length; i++) {
+        novoArray.add(arrayDuplicados[i]);
+    }
+
+    return novoArray;
+}
+
+console.log('Novo Set populado:');
+console.log(removeDuplicados(arrayDuplicados));
+
+console.log('*********************');
+
+// VERSAO DO INSTRUTOR
+
+const meuArray = [30, 30, 40, 5, 223, 2049, 3034, 5];
+
+function valoresUnicos(arr) {
+    const mySet = new Set(arr);
+
+    return [...mySet];
+}
+
+console.log(valoresUnicos(meuArray));
