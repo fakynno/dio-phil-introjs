a6642487cbe619d54db8deba21985e0724feb66a
fakynno <fakynno@gmail.com>
2022-05-14 20:44:45 -0300

    Logica para realizar a atividade

---
+++ philips/intro-javascript/aula 17 - exercicios praticos/script.js
@@ -0,0 +1,19 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+
+let quantidadePositivos = 0;
+
+for (let i = 0; i < 6; i++) {
+
+  const valorInformadoPeloUsuario = prompt('');
+
+  if (i > 0) {
+      quantidadePositivos += 1;
+  }
+
+// Completar os espaços em branco com uma possível solução para o desafio
+
+}
+
+console.log(`${quantidadePositivos} valores positivos`);
